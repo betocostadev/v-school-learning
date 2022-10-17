f357da8f3e30214074b22397b4b5ddd4505dc4ea
betocostadev <robertomarx@gmail.com>
2022-10-17 09:08:22 -0300

    some improvements to model and test cases

--- testing/tdd/tests/model.spec.js
+++ testing/tdd/tests/model.spec.js
@@ -15,12 +15,18 @@ test('model structure', () => {
 })
 
 describe('record', () => {
-  const heroes = [{name: 'Batman', name: 'Aquaman'}]
+  const heroes = [{ id: 1, name: 'Batman' }, { name: 'Aquaman' }]
 
   test('can add data to collection', () => {
     const model = new Model()
     model.record(heroes)
-    expect(model.$collection).toEqual(heroes)
+    expect(model.$collection).toEqual([
+      heroes[0],
+      {
+        id: expect.any(Number),
+        name: heroes[1].name
+      }
+    ])
   })
 
   test('gets called when data is passed to model', () => {
@@ -31,5 +37,42 @@ describe('record', () => {
     expect(model.$collection).toEqual(heroes)
     // Remove the spy
     spy.mockRestore()
+  })
+})
+
+describe('all', () => {
+  const villains = [{name: 'Joker'}, {name: 'Penguim'}]
+
+  test('returns empty model', () => {
+    const model = new Model()
+    expect(model.all()).toEqual([])
+  })
+
+  test('returns model data', () => {
+    const model = new Model(villains)
+    expect(model.all()).toEqual(villains)
+    expect(model.all().length).toBe(2)
+  })
+
+  test('original data stays intact', () => {
+    const model = new Model([{ name: 'Batman'}])
+    const data = model.all()
+    data[0].name = 'Joker'
+
+    expect(model.$collection[0].name).toBe('Batman')
+  })
+})
+
+describe('find', () => {
+  const vikings = [{ id: 1, name: 'Ragnar' }, { id: 2, name: 'Bjorn' }]
+  
+  test('returns null if nothing matches', () => {
+    const model = new Model()
+    expect(model.find('Batman')).toEqual(null)
+  })
+
+  test('find returns a matching entry', () => {
+    const model = new Model(vikings)
+    expect(model.find(1)).toEqual(vikings[0])
   })
 })
